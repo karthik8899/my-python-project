f057c3fd619ebdc7c5caa10f3da3e927785c89da
Karthik Kumar <kumarkarthik.g@gmail.com>
2025-02-26 21:16:19 -0600

    Added env

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+import os
+from dotenv import load_dotenv
 from image_helpers import get_image_pixels, render_pixels
 
+environment = os.getenv("ENVIRONMENT", "local")
+load_dotenv(f".env.{environment}")
+
+env_type = os.getenv("ENV_TYPE", "local")
+
+print(f"Environment: {env_type}")    
+
+
+# This function fetches pixel data from an image URL
 # Use the functions above to fetch pixel data and render the original data
 url = "https://www2.eecs.berkeley.edu/Faculty/Photos/Homepages/pamelafox.jpg"
 pixel_data = get_image_pixels(url)
